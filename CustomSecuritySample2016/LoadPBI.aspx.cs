591be83af37840f10ef78f1a56117689b51bed51
ruifeng.chang <ruifeng.chang@geely.com>
2020-01-10 11:45:47 +0800

    update

--- CustomSecuritySample2016/LoadPBI.aspx.cs
+++ CustomSecuritySample2016/LoadPBI.aspx.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.Samples.ReportingServices.CustomSecurity._10._86._65._20;
-using Microsoft.Reporting.WebForms;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using CustomSecuritySampleWeb.ServiceReferences;
 
 namespace CustomSecuritySampleWeb
 {
-    public partial class LoadPBI : BasePage
+    public partial class LoadPBI : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
